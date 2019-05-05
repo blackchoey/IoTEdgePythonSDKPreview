47ff14e08bd88cefdbbaafcb1e782b483b9a8b8d
Chaoyi Yuan <blackchoey@users.noreply.github.com>
2019-05-05 15:47:12 +0800

    initial commit

---
+++ modules/SampleModule/main.py
@@ -0,0 +1,90 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import time
+import os
+import sys
+import asyncio
+from six.moves import input
+import logging
+import threading
+from azure.iot.device.aio import IoTHubModuleClient
+from azure.iot.device import auth
+
+logging.basicConfig(level=logging.ERROR)
+
+# Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+PROTOCOL = "mqtt"
+
+async def main():
+    try:
+        print ( "\nPython %s\n" % sys.version )
+        print ( "IoT Hub Client for Python" )
+
+        print ( "Starting the IoT Hub Python sample using protocol %s..." % PROTOCOL)
+        print ( "The sample is now waiting for messages and will indefinitely. ")
+
+        # The "Authentication Provider" is the object in charge of creating authentication "tokens" for the device client.
+        auth_provider = auth.from_environment()
+        # For now, the SDK only supports MQTT as a protocol. the client object is used to interact with your Azure IoT hub.
+        # It needs an Authentication Provider to secure the communication with the hub, using either tokens or x509 certificates
+        module_client = IoTHubModuleClient.from_authentication_provider(auth_provider, PROTOCOL)
+
+        # connect the client.
+        await module_client.connect()
+
+        # define behavior for receiving an input message on input1
+        async def input1_listener(module_client):
+            while True:
+                input_message = await module_client.receive_input_message("input1")  # blocking call
+                print("the data in the message received on input1 was ")
+                print(input_message.data)
+                print("custom properties are")
+                print(input_message.custom_properties)
+                print("forwarding mesage to output1")
+                await module_client.send_to_output(input_message, "output1")
+
+        # define behavior for halting the application
+        def stdin_listener():
+            while True:
+                try:
+                    selection = input("Press Q to quit\n")
+                    if selection == "Q" or selection == "q":
+                        print("Quitting...")
+                        break
+                except:
+                    time.sleep(10)
+
+        # Schedule task for C2D Listener
+        listeners = asyncio.gather(input1_listener(module_client))
+
+        # Run the stdin listener in the event loop
+        loop = asyncio.get_event_loop()
+        user_finished = loop.run_in_executor(None, stdin_listener)
+
+        # Wait for user to indicate they are done listening for messages
+        await user_finished
+
+        # Cancel listening
+        listeners.cancel()
+
+        # Finally, disconnect
+        await module_client.disconnect()
+
+    except Exception as e:
+        print ( "Unexpected error %s " % e )
+        return
+
+if __name__ == "__main__":
+    # asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
